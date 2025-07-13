87c00c05dd01060b827b191225668b0a78d15e50
WTF619 <kabboislam126@gmail.com>
2025-07-13 10:33:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add a personalized welcome message
+window.onload = function () {
+    const name = prompt("Welcome to Our Futsal Ground! What's your name?");
+    if (name) {
+        alert(`Hi ${name}, explore and enjoy Our Futsal Ground!`);
+    }
+};
